f93e376ae4dac2567acc18a968b935c7eede90d1
Calixte Denizet <calixte.denizet@gmail.com>
2017-05-22 18:58:07 +0200

    Add a scheduler

---
+++ bin/schedule.py
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from spikes import models
+import logging
+
+
+logging.basicConfig()
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=30)
+def timed_job():
+    models.update()
+
+
+sched.start()
