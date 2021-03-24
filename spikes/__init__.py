6f98e83100d5a1d2cb3e5e9dac2557fde81be5d8
Calixte Denizet <calixte.denizet@gmail.com>
2021-03-24 22:47:33 +0100

    Workaround for heroku issue with SQLAlchemy

--- spikes/__init__.py
+++ spikes/__init__.py
@@ -9,7 +9,12 @@ import os
 
 
 app = Flask(__name__, template_folder='../templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+uri = os.getenv('DATABASE_URL', config.get_database())
+# Workaround for Heroku
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 cors = CORS(app)
